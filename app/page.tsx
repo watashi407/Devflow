36293b90d2825c332ce63c87ad1d54eda391ffb6
watashi407 <hyperteddy70@gmail.com>
2024-11-30 17:16:32 +0800

    Add Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-light-900">
-      Welcome to the world of Nextjs!
-    </h1>
+    <>
+      <h1 className="font-space-grotesk text-3xl">
+        Welcome to the world of Nextjs! (space grotesk)
+      </h1>
+      <h1 className="font-inter text-3xl">
+        Welcome to the world of Nextjs! (inter)
+      </h1>
+    </>
   );
 }
