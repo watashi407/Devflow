6afcc21b023c99da9d63e063e4a29b8701b0566c
dakuOten <alvin.gultiano@evsu.edu.ph>
2024-11-30 13:22:31 +0800

    Setup eslint and prettier in vscode for best practices

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Welcome to the world of Nextjs!</h1>;
+  return (
+    <h1 className="text-3xl font-black text-white">
+      Welcome to the world of Nextjs!
+    </h1>
+  );
 }
