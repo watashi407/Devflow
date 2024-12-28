9f55b245c83c633756cb88db118de29462bb1671
watashi407 <hyperteddy70@gmail.com>
2024-12-28 15:25:56 +0800

    Implementing Dynamic Routes for Api Account v2

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -13,13 +13,13 @@ export async function GET(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  if (!id) throw new NotFoundError("User");
+  if (!id) throw new NotFoundError("Account");
 
   try {
     await dbConnect();
 
     const account = await Account.findById({ _id: id });
-    if (!account) throw new NotFoundError("User");
+    if (!account) throw new NotFoundError("Account");
 
     return NextResponse.json({ success: true, data: account }, { status: 200 });
   } catch (error) {
@@ -34,7 +34,7 @@ export async function PUT(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  if (!id) throw new NotFoundError("User");
+  if (!id) throw new NotFoundError("Account");
 
   try {
     await dbConnect();
