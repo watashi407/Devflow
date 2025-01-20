3196a83e3c8d0384bacd4efa9e9997c9b25d1472
watashi407 <hyperteddy70@gmail.com>
2025-01-20 19:14:36 +0800

    NEXT PUBLIC API

--- lib/api.ts
+++ lib/api.ts
@@ -5,8 +5,9 @@ import { IUser } from "@/database/user.model";
 import { fetchHandlers } from "./handlers/fetch";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "https://devflow-sigma-ten.vercel.app";
+  process.env.NODE_ENV === "production"
+    ? "https://devflow-sigma-ten.vercel.app/api"
+    : "http://localhost:3000/api";
 export const api = {
   auth: {
     oAuthSignIn: ({
